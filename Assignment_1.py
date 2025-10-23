44a37207933281b0ba2f41c0086f73b832c4e6e6
jinglan Liang <jinglanliang10@gmail.com>
2025-10-23 16:58:19 -0600

    added Dictionary

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,18 +1,14 @@
 print("Welcome to the Grade Registry Program")
-run_program = True
-student_name = []
-student_gpa = []
-while run_program:
+student_gpa = dict()
+while True:
     print("\nWould you like to add a new student? y(yes),n(no)\n")
     confirmation_input = str(input(""))
     confirmation_input = confirmation_input.lower()
     if confirmation_input == 'y' or confirmation_input == 'yes':
         print("\nEnter the student's name:\n")
         input_name = str(input(""))
-        student_name.append(input_name)
         print("\nEnter student GPA for each subject. Enter -1 to stop entering GPA\n")
-        count = 0
-        sum = 0.0
+        gpa = []
         while True:
             gpa_input = float(input(""))
             if gpa_input == -1:
@@ -20,17 +16,16 @@ while run_program:
             elif gpa_input > 4 or gpa_input < 0:
                 print("\ninvalid input\n")
             else:
-                sum+=gpa_input
-                count+=1
-        if count != 0:     
-            average = sum/count
-            student_gpa.append(average)
+                gpa.append(gpa_input)
+        if len(gpa) != 0:     
+            average_gpa = sum(gpa)/len(gpa)
         else:
-            student_gpa.append(0)
+            average_gpa = 0
+        student_gpa[input_name] = average_gpa
     elif confirmation_input == 'n' or confirmation_input == 'no':
-        run_program = False
+        break
     else:
         print("\nIncorrect Input, please enter y(yes)/n(no)\n")
 print("This is the list of students in the system, and their corresponding accumulative GPA")
-for name, gpa in zip(student_name, student_gpa):
+for name,gpa in student_gpa.items():
         print(name, format(gpa, ".2f"))
